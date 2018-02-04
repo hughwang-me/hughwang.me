f0b1ad7bbe97ca4e26603530f4e52f47e158de85
Hugh Wang <18501667737@163.com>
2018-02-04 23:55:00 +0800

    auto by shell

--- src/root.js
+++ src/root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 require('./css/pc_base.css');
+require('./css/mob_base.css');
 
 import PcIndex from './js/pc_index';
 import PcIndex from './js/mob_index';
