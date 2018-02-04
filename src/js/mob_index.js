1fdd8646d4f1a04f3a6ff3e679b554a4ea58d240
Hugh Wang <18501667737@163.com>
2018-02-04 23:58:21 +0800

    auto by shell

--- src/js/mob_index.js
+++ src/js/mob_index.js
@@ -28,7 +28,7 @@ class MobIndex extends React.Component {
                     className="header"
                 >
                     <div className="title">
-                        王欢的博客
+                        手机端
                     </div>
                     {/*<Menu*/}
                         {/*theme="dark"*/}
@@ -42,60 +42,6 @@ class MobIndex extends React.Component {
                     {/*</Menu>*/}
                 </Header>
 
-                <Layout >
-                    <Sider
-                        className='sider'
-                        trigger={null}
-                        width={200}
-                        style={{ background: '#fff' }}
-                        collapsible
-                        collapsed={this.state.collapsed}
-                        onCollapse={this.onCollapse.bind(this)}
-                    >
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{ height: '100%', borderRight: 2 }}
-                        >
-                            <Menu.Item key="1">
-                                <Icon type="pie-chart" />
-                                <span>Option 1</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <Icon type="desktop" />
-                                <span>Option 2</span>
-                            </Menu.Item>
-                            <SubMenu
-                                key="sub1"
-                                title={<span><Icon type="user" /><span>User</span></span>}
-                            >
-                                <Menu.Item key="3">Tom</Menu.Item>
-                                <Menu.Item key="4">Bill</Menu.Item>
-                                <Menu.Item key="5">Alex</Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Layout className={'content_layout'}>
-                        <Breadcrumb style={{ margin: '16px 0' }}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-                            Content<br/><br/><br/><br/><br/><br/>
-                            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                            <br/><br/><br/>1
-                            <br/><br/><br/><br/><br/><br/>
-                            2
-                            <br/><br/><br/><br/>
-                            <br/><br/><br/>v
-                        </Content>
-                        <Footer>Copyright 2018 © All Rights Reserved</Footer>
-                    </Layout>
-                    {/*<Sider>right sidebar</Sider>*/}
-                </Layout>
 
             </Layout>
         )
